67cc5101fdf3e11ea1a48c32e1580676c576a20b
flyingbucket <menglinli598@gmail.com>
2025-11-06 19:05:04 +0800

    control structures

---
+++ control_structures.scala
@@ -0,0 +1,12 @@
+println("if-else statments")
+val x:Int = 100
+println(s"x:${x}")
+if (x>0) {
+  println("Positive")
+} else if (x<0){
+  println("Negative")
+} else {
+  println("Zero")
+}
+
+println("for loops")
